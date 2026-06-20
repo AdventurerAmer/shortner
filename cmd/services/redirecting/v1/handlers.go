0733740fc395365deb71714b5793b6a28a1460aa
AdventurerAmer <ahamerdev@gmail.com>
2026-06-20 16:58:44 +0300

    redirecting work for browers...

--- cmd/services/redirecting/v1/handlers.go
+++ cmd/services/redirecting/v1/handlers.go
@@ -3,8 +3,10 @@ package v1
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/AdventurerAmer/shortner/config"
+	"github.com/AdventurerAmer/shortner/errs"
 	"github.com/AdventurerAmer/shortner/internal/core/ports"
 	"github.com/AdventurerAmer/shortner/logging"
 	"github.com/AdventurerAmer/shortner/web"
@@ -34,8 +36,15 @@ func (h *Handlers) Redirect(c *web.Context) (any, error) {
 
 	resp, err := h.srv.Redirect(ctx, req)
 	if err != nil {
+		// TODO: html templates for not-found and err
+		if errs.IsNotFound(err) {
+		} else {
+		}
 		return nil, fmt.Errorf("'srv.Redirect' failed: %w", err)
 	}
 
-	return resp, nil
+	// http.StatusFound represents a temporary (302) redirect
+	http.Redirect(c.ResponseWriter, c.Request, resp.LongURL, http.StatusFound)
+
+	return nil, nil
 }
